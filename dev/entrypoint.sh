1011e6ea5e6cdf57740bf1a7cf23666481a9c991
David Zaharee <david@zaharee.com>
2015-08-16 00:12:30 -0400

    dev: use uid/gid that are unlikely to collide
    Running "usermod -u <uid> <user>" not only changes the user's UID, but
    also all the files in the user's home directory with that UID. To
    minimize the risk of this we're using a large uid/gid that is outside
    the typical range one would find on most systems.

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -2,6 +2,7 @@
 
 DEV_USER=dev
 DEV_GROUP=dev
+DEV_ORIGINAL_GID=913379
 CMD=${*:-/bin/bash}
 
 if [ -n "${DEV_UID}" ]; then
@@ -10,6 +11,7 @@ fi
 
 if [ -n "${DEV_GID}" ]; then
     groupmod --gid ${DEV_GID} ${DEV_GROUP} > /dev/null 2>&1
+    find / -gid ${DEV_ORIGINAL_GID} -exec chgrp -h ${DEV_GID} {} \; > /dev/null 2>&1
 fi
 
 exec sudo -u ${DEV_USER} ${CMD}
